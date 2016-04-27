3a0ccc8ed93f99d567976c9e3a0778130390dd43
Oliver Short <oshort55@gmail.com>
2016-04-27 14:57:38 -0400

    Initial Commit

---
+++ InOurPrime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InOurPrime
+//
+//  Created by Oliver Short on 4/27/16.
+//  Copyright © 2016 Oliver Short. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InOurPrime/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InOurPrime
+//
+//  Created by Oliver Short on 4/27/16.
+//  Copyright © 2016 Oliver Short. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
